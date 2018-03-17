15a18fcfa12b07582e4c9ab3d5ca5978f2ea5638
patrzykont <aaronpatrzykont@Aarons-MacBook-Air.local>
2018-03-16 20:06:29 -0500

    update

---
+++ shoppingCart/shopping-cart/public/javascripts/vue.js
@@ -0,0 +1,23 @@
+var vm = new Vue({
+  el: '#vue-instance',
+  data: {
+    userInput: 0,
+    randomNumber: 5,
+  },
+  methods: {
+    getRandomNumber: function(min, max) {
+      return Math.floor(Math.random() * (max -min +1)) + min;
+    },
+  },
+  computed: {
+    message: function() {
+      if (this.userInput == this.randomNumber) {
+        this.randomNumber = this.getRandomNumber(1, 10);
+        return 'You got it right!';
+      } else {
+        this.randomNumber = this.getRandomNumber(1, 10);
+        return 'Try Again';
+      }
+    }
+  },
+});
